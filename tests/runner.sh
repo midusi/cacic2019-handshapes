0e5466651f8e1a5543fb56612414a57652c39af1
ulises-jeremias <ulisescf.24@gmail.com>
2020-03-08 18:25:19 -0300

    Updated tests

---
+++ tests/runner.sh
@@ -0,0 +1,28 @@
+# DONT EXECUTE THIS CODE!!
+# Just include to load tests cases
+# by defining ROOT dir
+
+has_param() {
+    local term="$1"
+    shift
+    for arg; do
+        if [[ $arg == "$term" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+if has_param '--show' "$@"; then
+    echo "Tests list:"
+    echo
+    for i in "${!tests[@]}"; do 
+        printf "\t%s\t%s\n" "$i" "${tests[$i]}"
+    done
+    exit 0
+fi
+
+for test in "${tests[@]}"; do
+    printf "Running test \t%s\n" $test
+    $test
+done
