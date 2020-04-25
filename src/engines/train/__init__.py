1afbb7d899d1146d2448103a38e16917f393f86b
ulises-jeremias <ulisescf.24@gmail.com>
2020-04-25 09:03:47 -0300

    Updated maml

--- src/engines/train/__init__.py
+++ src/engines/train/__init__.py
@@ -1,14 +1,15 @@
+import tensorflow as tf
 from src.engines.steps import steps
 
-from .train import train as normal_train
-from .maml import train as maml_train
+from .train import train
 
 def train(*args, **kwargs):
-    train_step, test_step = steps(*args, **kwargs)
+    model = kwargs.get('model')
+    model_copy = tf.keras.models.clone_model(model) if kwargs.get('engine') == 'maml' else None
+
+    train_step, test_step = steps(*args, model_copy=None, **kwargs)
     
-    train_step=kwargs.get('train_step', train_step)
-    test_step=kwargs.get('test_step', test_step)
+    kwargs['train_step'] = kwargs.get('train_step', train_step)
+    kwargs['test_step'] = kwargs.get('test_step', test_step)
 
-    train_engine = maml_train if kwargs.get('engine') == 'maml' else normal_train
-
-    return train_engine(train_step=train_step, test_step=test_step, **kwargs) 
+    return train(*args, **kwargs)
