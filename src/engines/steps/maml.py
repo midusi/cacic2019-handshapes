741457b7d454149fa1d924ee9fab20a1b530efc1
ulises-jeremias <ulisescf.24@gmail.com>
2020-04-27 15:33:29 -0300

    Updated

--- src/engines/steps/maml.py
+++ src/engines/steps/maml.py
@@ -1,17 +1,30 @@
 import tensorflow as tf
 
-def steps(model, loss_object, optimizer, train_loss, train_accuracy, test_loss, test_accuracy, **kwargs):
+def steps(model, loss_object, optimizer, train_loss, train_accuracy, test_loss, test_accuracy, copied_model, **kwargs):
 
-    @tf.function
     def train_step(images, labels):
-        with tf.GradientTape() as tape:
-            predictions = model(tf.cast(images, tf.float32), training=True)
-            loss = loss_object(labels, predictions)
-        gradients = tape.gradient(loss, model.trainable_variables)
+        with tf.GradientTape() as outer_tape:
+            # outer_tape.watch(model.trainable_variables)
+            
+            with tf.GradientTape() as inner_tape:
+                predictions = model(tf.cast(images, tf.float32), training=True)
+                inner_loss = loss_object(labels, predictions)
+            
+            copied_model.set_weights(model.get_weights())
+            gradients = inner_tape.gradient(inner_loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, copied_model.trainable_variables))
+
+            train_loss(inner_loss)
+            train_accuracy(labels, predictions)
+
+        predictions = copied_model(tf.cast(images, tf.float32), training=False)
+        outer_loss = loss_object(labels, predictions)
+
+        gradients = outer_tape.gradient(outer_loss, copied_model.trainable_variables)
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-        train_loss(loss)
-        train_accuracy(labels, predictions)
+        test_loss(outer_loss)
+        test_accuracy(labels, predictions)
 
     @tf.function
     def test_step(images, labels):
--- src/engines/train/__init__.py
+++ src/engines/train/__init__.py
@@ -1,14 +1,15 @@
-from src.engines.steps import steps
+import tensorflow as tf
 
-from .train import train as normal_train
-from .maml import train as maml_train
+from src.engines.steps import steps
+from .train import train
 
 def train(*args, **kwargs):
-    train_step, test_step = steps(*args, **kwargs)
+    model = kwargs.get('model')
+    copied_model = tf.keras.models.clone_model(model) if kwargs.get('engine') == 'maml' else None
+
+    train_step, test_step = steps(*args, copied_model=copied_model, **kwargs)
     
-    train_step=kwargs.get('train_step', train_step)
-    test_step=kwargs.get('test_step', test_step)
+    kwargs['train_step'] = kwargs.get('train_step', train_step)
+    kwargs['test_step'] = kwargs.get('test_step', test_step)
 
-    train_engine = maml_train if kwargs.get('engine') == 'maml' else normal_train
-
-    return train_engine(train_step=train_step, test_step=test_step, **kwargs)
+    return train(*args, **kwargs)
