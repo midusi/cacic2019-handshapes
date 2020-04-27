3e4361b833fa2608f6de3936fccf6ae51bf2a81a
ulises-jeremias <ulisescf.24@gmail.com>
2020-04-26 21:49:13 -0300

    Updated

--- src/engines/train/maml.py
+++ src/engines/train/maml.py
@@ -40,8 +40,8 @@ def train(model=None, epochs=10, batch_size=32, format_paths=True,
                         inner_loss = loss_object(labels, predictions)
                     
                     copied_model.set_weights(model.get_weights())
-                    gradients = inner_tape.gradient(inner_loss, copied_model.trainable_variables)
-                    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+                    gradients = inner_tape.gradient(inner_loss, model.trainable_variables)
+                    optimizer.apply_gradients(zip(gradients, copied_model.trainable_variables))
 
                     train_loss(inner_loss)
                     train_accuracy(labels, predictions)
@@ -49,7 +49,7 @@ def train(model=None, epochs=10, batch_size=32, format_paths=True,
                 predictions = copied_model(tf.cast(images, tf.float32), training=False)
                 outer_loss = loss_object(labels, predictions)
 
-            gradients = outer_tape.gradient(outer_loss, copied_model.trainable_variables)
+            gradients = outer_tape.gradient(outer_loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
             test_loss(outer_loss)
