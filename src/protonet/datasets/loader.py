67a26899ec489cbaec3076541081ad3305a83f54
ulises-jeremias <ulisescf.24@gmail.com>
2020-05-09 18:14:01 -0300

    Added mapping from list to np array

--- src/protonet/datasets/loader.py
+++ src/protonet/datasets/loader.py
@@ -92,8 +92,8 @@ def load(data_dir, config, splits):
                x[index, :, :, :] = datagen.apply_transform(x[index], datagen_args)
 
         data = np.split(x[:, 1], np.cumsum(np.unique(x[:, 0], return_counts=True)[1])[:-1])
-        
-        data_loader = DataLoader(data,
+
+        data_loader = DataLoader(np.array(data),
                                  n_classes=nb_classes,
                                  n_way=n_way,
                                  n_support=n_support,
